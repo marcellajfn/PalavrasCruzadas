32a82532a443415dd0c7b4d630104685c35fc2b3
Marcella Nunes <marcellajfn@outlook.com>
2013-11-01 10:29:43 -0200

    incluindo a impressão da matriz depois de terminado o loop for

--- palavrasCruzadas/PalavrasCruzadas.java
+++ palavrasCruzadas/PalavrasCruzadas.java
@@ -8,6 +8,8 @@ public class PalavrasCruzadas {
 	int [][] matriz = new int [Nlinhas][Ncolunas];
 	int i; // linhas
 	int j; //colunas
+	
+	
 	void palavras (){
 		int count =0;
 		for (i=0;i<=Nlinhas; i++){
@@ -16,26 +18,26 @@ public class PalavrasCruzadas {
 				if (matriz [i][j] == -1) 
 						matriz[i][j]= -1;				
 				
-				else {
-				
 				if ((matriz [i][j] != -1) && matriz [i][j+1]==0 || matriz [i+1][j] ==0 || matriz [i-11][j] ==0){
 					matriz[i][j]= 1 + count;
 					count = count+1;
-				}
-				 
-				if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i][j-1] !=0) || (matriz [i][j-1]!= -1)))
-							matriz [i][j+count] = 0;	
-						
-				if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i][j+1] !=0 || matriz [i][j+1]!= -1)))
-							matriz [i][j+count] = 0;
-					
-				if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i-1][j] !=0) || (matriz [i-1][j]!= -1)))
-						matriz [i][j+count] = 0;	
-					
-				if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i+1][j] !=0 || matriz [i+1][j]!= -1)))
-						matriz [i][j+count] = 0;
+				
+						if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i][j-1] !=0) || (matriz [i][j-1]!= -1)))
+									matriz [i][j+count+1] = 0;	
+								
+						if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i][j+1] !=0 || matriz [i][j+1]!= -1)))
+									matriz [i][j+count+1] = 0;
+							
+						if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i-1][j] !=0) || (matriz [i-1][j]!= -1)))
+								matriz [i][j+count+1] = 0;	
+							
+						if (((matriz [i][j] !=0) || matriz [i][j]!= -1) && ((matriz [i+1][j] !=0 || matriz [i+1][j]!= -1)))
+								matriz [i][j+count+1] = 0;
+					}
 				}
 			}
+		
+			System.out.println("matriz[i][j]");
 		}
 	}
-}
+
